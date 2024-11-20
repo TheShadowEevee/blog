61b41ceceb071804f50454ae040db53083b6c4d0
Lumi <theshadoweevee@shad.moe>
2024-11-20 12:07:16 -0500

    Update RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,9 @@
-import rss from "@astrojs/rss";
-import { getSortedPosts } from "@utils/content-utils";
-import { url } from "@utils/url-utils";
-import type { APIContext } from "astro";
-import MarkdownIt from "markdown-it";
-import sanitizeHtml from "sanitize-html";
-import { siteConfig } from "@/config";
+import { siteConfig } from '@/config'
+import rss from '@astrojs/rss'
+import type { APIContext } from 'astro';
+import { getSortedPosts } from '@utils/content-utils'
+import MarkdownIt from 'markdown-it'
+import sanitizeHtml from 'sanitize-html'
 
 const parser = new MarkdownIt();
 
@@ -19,24 +18,30 @@ function stripInvalidXmlChars(str: string): string {
 export async function GET(context: APIContext) {
 	const blog = await getSortedPosts();
 
-	return rss({
-		title: siteConfig.title,
-		description: siteConfig.subtitle || "No description",
-		site: context.site ?? "https://fuwari.vercel.app",
-		items: blog.map((post) => {
-			const content =
-				typeof post.body === "string" ? post.body : String(post.body || "");
-			const cleanedContent = stripInvalidXmlChars(content);
-			return {
-				title: post.data.title,
-				pubDate: post.data.published,
-				description: post.data.description || "",
-				link: url(`/posts/${post.slug}/`),
-				content: sanitizeHtml(parser.render(cleanedContent), {
-					allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-				}),
-			};
-		}),
-		customData: `<language>${siteConfig.lang}</language>`,
-	});
+  return rss({
+    xmlns: {
+      dc: `http://purl.org/dc/elements/1.1/`,
+      content: `http://purl.org/rss/1.0/modules/content/`,
+      atom: `http://www.w3.org/2005/Atom`,
+    },
+    title: siteConfig.title,
+    description: siteConfig.subtitle || 'No description',
+    site: context.site ?? 'https://blog.shad.moe',
+    items: blog.map(post => {
+      return {
+        title: post.data.title,
+        pubDate: post.data.published,
+        description: post.data.description || '',
+        link: `/posts/${post.slug}/`,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        }),
+      }
+    }),
+    customData:
+      `<language>${siteConfig.lang}</language>` +
+      `<lastBuildDate>${blog[0]!.data.published.toUTCString()}</lastBuildDate>` +
+      `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml"/>` +
+      `<pubDate>${blog[0]!.data.published.toUTCString()}</pubDate>`,
+  })
 }
