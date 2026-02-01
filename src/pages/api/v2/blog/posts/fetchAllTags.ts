4c5977d23bc761f023a00a95612cc2c9e569e7c3
TheShadowEevee <lumi.malloy@shad.moe>
2026-01-31 20:01:21 -0500

    Correct API Pathing issue

--- src/pages/api/v2/blog/posts/fetchAllTags.ts
+++ src/pages/api/v2/blog/posts/fetchAllTags.ts
@@ -1,1 +1,47 @@
-./fetchAllPosts
+import type { Post } from '@/types/posts';
+import type { APIRoute } from 'astro';
+import { GET as getPosts } from './fetchAllPosts';
+
+export const prerender = false;
+
+export type Category = {
+	name: string;
+	count: number;
+};
+
+export const GET: APIRoute = async (Astro) => {
+	try {
+		const initResponse = await getPosts(Astro);
+		const response = await initResponse.json();
+
+		let postList: Post[] = [];
+
+		if (response.success === true) {
+			postList = response.result;
+		}
+		const tags: string[] = new Array();
+
+		for (const rkey in postList) {
+			for (const tag of postList[rkey].extendedData?.tags ?? []) {
+				tags.push(tag);
+			}
+		}
+
+		return new Response(
+			JSON.stringify({
+				success: true,
+				result: tags,
+			}),
+			{
+				status: 200,
+			}
+		);
+	} catch (error) {
+		return new Response(
+			JSON.stringify({
+				success: false,
+				result: `Failed to get data: ${error}`,
+			})
+		);
+	}
+};
