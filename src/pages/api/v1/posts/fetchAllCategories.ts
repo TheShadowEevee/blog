a97d5339c355d2d3cf72e880f3a093f24c59b03c
TheShadowEevee <lumi.malloy@shad.moe>
2026-01-31 20:59:31 -0500

    Fix damaged files (accidental Symlink??)

--- src/pages/api/v1/posts/fetchAllCategories.ts
+++ src/pages/api/v1/posts/fetchAllCategories.ts
@@ -1,1 +1,66 @@
-./fetchAllPosts
+import type { Post } from '@/types/posts';
+import I18nKey from '@i18n/i18nKey';
+import { i18n } from '@i18n/translation';
+import type { APIRoute } from 'astro';
+import { GET as getPosts } from './fetchAllPosts';
+
+export const prerender = false;
+
+export type Category = {
+    name: string;
+    count: number;
+};
+
+export const GET: APIRoute = async (Astro) => {
+    try {
+        const count: { [key: string]: number } = {};
+
+        const initResponse = await getPosts(Astro);
+        const response = await initResponse.json();
+
+        let postList: Post[] = [];
+
+        if (response.success === true) {
+            postList = response.result;
+        }
+
+        for (const rkey in postList) {
+            if (!postList[rkey].extendedData?.category) {
+                const ucKey = i18n(I18nKey.uncategorized);
+                count[ucKey] = count[ucKey] ? count[ucKey] + 1 : 1;
+            } else {
+                count[postList[rkey].extendedData?.category] = count[
+                    postList[rkey].extendedData?.category
+                ]
+                ? count[postList[rkey].extendedData?.category] + 1
+                : 1;
+            }
+        }
+
+        const lst = Object.keys(count).sort((a, b) => {
+            return a.toLowerCase().localeCompare(b.toLowerCase());
+        });
+
+        const ret: Category[] = [];
+        for (const c of lst) {
+            ret.push({ name: c, count: count[c] });
+        }
+
+        return new Response(
+            JSON.stringify({
+                success: true,
+                result: ret,
+            }),
+            {
+                status: 200,
+            }
+        );
+    } catch (error) {
+        return new Response(
+            JSON.stringify({
+                success: false,
+                result: [{ name: 'Failed to get categories.', count: 0 }],
+            })
+        );
+    }
+};
---
+++ src/pages/api/v1/posts/fetchAllPosts.ts
@@ -0,0 +1,64 @@
+import type { MarkdownPost, Profile } from '@/types/posts';
+import { getProfile, safeFetch } from '@utils/content-utils';
+import { parse } from '@utils/parser';
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const GET: APIRoute = async () => {
+    try {
+        const profile: Profile = await getProfile();
+
+        const response = await safeFetch(
+            `${profile.pds}/xrpc/com.atproto.repo.listRecords?repo=${profile.did}&collection=com.whtwnd.blog.entry`
+        );
+
+        const allPosts: Map<string, MarkdownPost> = new Map();
+        for (const data of response.records) {
+            const matches = data.uri.split('/');
+            const rkey = matches[matches.length - 1];
+            const record = data.value;
+            if (
+                matches &&
+                matches.length === 5 &&
+                record &&
+                (record.visibility === 'public' || !record.visibility) // If no visibility field, assume public
+            ) {
+                allPosts.set(rkey, {
+                    title: record.title,
+                    createdAt: new Date(record.createdAt),
+                             mdcontent: record.content,
+                             rkey,
+                             visibility: record.visibility,
+                             ogp: record.ogp,
+                             data: '',
+                });
+            }
+        }
+        return new Response(
+            JSON.stringify({
+                success: true,
+                // @ts-ignore : This is totally defined before use, if not womp womp
+                result: Object.fromEntries(
+                    new Map(
+                        [...(await parse(allPosts)).entries()].sort(
+                            (a, b) =>
+                            new Date(b[1].extendedData?.published ?? 0).getTime() -
+                            new Date(a[1].extendedData?.published ?? 0).getTime()
+                        )
+                    )
+                ),
+            }),
+            {
+                status: 200,
+            }
+        );
+    } catch (error) {
+        return new Response(
+            JSON.stringify({
+                success: false,
+                result: `Failed to get data: ${error}`,
+            })
+        );
+    }
+};
