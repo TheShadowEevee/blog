f6ad96dce24dfb4e31c1c604a3bedb11621bd6ce
TheShadowEevee <lumi.malloy@shad.moe>
2026-01-31 20:54:29 -0500

    More API Updates, Update Caching

---
+++ src/pages/api/v2/blog/cache/blob.ts
@@ -0,0 +1,79 @@
+import type { APIRoute } from 'astro';
+import Redis from 'ioredis';
+
+export const prerender = false;
+
+const redis = new Redis({
+	host: import.meta.env.REDIS_IP, // Local Redis server IP
+	port: import.meta.env.REDIS_PORT, // Local Redis server port
+	password: import.meta.env.REDIS_PASSWORD, // Optional, if your Redis instance requires authentication
+	maxRetriesPerRequest: 1,
+});
+
+redis.on('error', (err) => {
+	console.error('Redis error:', err.message);
+	redis.disconnect();
+});
+
+export const GET: APIRoute = async ({ url }) => {
+	try {
+		const blobId = url.searchParams.get('id');
+		const blobType = url.searchParams.get('type');
+
+		const result = await redis.get(`blog:${blobType}:${blobId}`);
+
+		if (!result) {
+			return new Response(
+				`Resource '${blobId}' with type '${blobType}' does not exist in the cache.`,
+				{ status: 404 }
+			);
+		} else {
+			return new Response(JSON.stringify(result), { status: 200 });
+		}
+	} catch (error) {
+		console.log(error);
+		return new Response(
+			'An error occurred while processing your query. Please try again later.',
+			{ status: 500 }
+		);
+	}
+};
+
+export const POST: APIRoute = async ({ request, url }) => {
+	try {
+		const blobId = url.searchParams.get('id');
+		const blobType = url.searchParams.get('type');
+
+		if (request.headers.get('Content-Type') === 'application/json' && blobId && blobType) {
+			const body = await request.json();
+
+			const result = await redis.set(
+				`blog:${blobType}:${blobId}`,
+				JSON.stringify(body),
+				'EX',
+				import.meta.env.POST_CACHE_SECONDS
+			);
+
+			return new Response(
+				`Data has been successfully cached for ${import.meta.env.POST_CACHE_SECONDS} seconds.`,
+				{
+					status: 200,
+				}
+			);
+		} else if (blobId && blobType) {
+			return new Response("The parameters 'type' and 'id' must be provided.", {
+				status: 400,
+			});
+		} else if (request.headers.get('Content-Type') === 'application/json') {
+			return new Response("Content-Type must be 'application/json'", { status: 400 });
+		} else {
+			return new Response(null, { status: 400 });
+		}
+	} catch (error) {
+		console.log(error);
+		return new Response(
+			'An error occurred while processing your query. Please try again later.',
+			{ status: 500 }
+		);
+	}
+};
---
+++ src/pages/api/v2/blog/protocols/atproto/entry/[id].ts
@@ -0,0 +1,111 @@
+import type { MarkdownPost, Post, Profile } from '@/types/posts';
+import { getProfile, safeFetch } from '@utils/content-utils';
+import { parse } from '@utils/parser';
+import type { APIRoute } from 'astro';
+import { GET as cacheGET } from '../../../cache/blob';
+
+export const prerender = false;
+
+export const GET: APIRoute = async (Astro) => {
+	try {
+		const rkey = Astro.params.rkey;
+
+		if (rkey) {
+			// https://stackoverflow.com/a/75664821
+			const domain = Astro.request.url.match(
+				/^(?<protocol>https?:\/\/)(?=(?<fqdn>[^:/]+))(?:(?<service>www|ww\d|cdn|ftp|mail|pop\d?|ns\d?|git)\.)?(?:(?<subdomain>[^:/]+)\.)*(?<domain>[^:/]+\.?[a-z0-9]+)(?::(?<port>\d+))?(?<path>\/[^?]*)?(?:\?(?<query>[^#]*))?(?:#(?<hash>.*))?/i
+			);
+
+			const cacheURL = `${
+				(domain?.groups?.protocol ?? '') +
+				(domain?.groups?.fqdn ?? '') +
+				(domain?.groups?.port ? `:${domain?.groups?.port}` : '')
+			}/api/v2/blog/cache/blob?type=entry&id=${rkey}`;
+
+			const initResponse = await cacheGET(Astro);
+
+			if (initResponse.status == 200) {
+				const response = await initResponse.json();
+
+				return new Response(
+					JSON.stringify({
+						success: true,
+						result: JSON.parse(response.result),
+					})
+				);
+			} else {
+				const profile: Profile = await getProfile();
+
+				const postResponse = await safeFetch(
+					`${profile.pds}/xrpc/com.atproto.repo.getRecord?repo=${profile.did}&collection=com.whtwnd.blog.entry&rkey=${rkey}`
+				);
+
+				if (!postResponse.error) {
+					const mdposts: Map<string, MarkdownPost> = new Map();
+					let post: Map<string, Post>;
+
+					const matches = postResponse.uri.split('/');
+					const rkey = matches[matches.length - 1];
+					const record = postResponse.value;
+					if (
+						matches &&
+						matches.length === 5 &&
+						record &&
+						(record.visibility === 'public' ||
+							record.visibility === 'url' ||
+							!record.visibility)
+					) {
+						mdposts.set(rkey, {
+							title: record.title,
+							createdAt: new Date(record.createdAt),
+							mdcontent: record.content,
+							rkey,
+							visibility: record.visibility,
+							ogp: record.ogp,
+							data: '',
+						});
+
+						post = await parse(mdposts);
+
+						if (record.visibility === 'public') {
+							// It would be nice to get rid of this, but "cachePOST" doesn't seem to work atm.
+							const rkeyPost = await fetch(cacheURL, {
+								method: 'POST',
+								headers: { 'Content-Type': 'application/json' },
+								body: JSON.stringify(post.get(rkey)),
+							});
+
+							if (!rkeyPost.status == 200) {
+								console.log(
+									`Error caching the post. Check the logs on the API server for more information.`
+								);
+							}
+						}
+
+						return new Response(
+							JSON.stringify({
+								success: true,
+								result: post.get(rkey),
+							})
+						);
+					}
+				}
+				return new Response(
+					JSON.stringify({
+						success: false,
+						result: 'Unknown Error Fetching Post',
+					}),
+					{ status: 400 }
+				);
+			}
+		}
+		throw "Entries 'id' is null or undefined";
+	} catch (error) {
+		return new Response(
+			JSON.stringify({
+				success: false,
+				result: `Failed to get data: ${error}`,
+			})
+		);
+	}
+};
